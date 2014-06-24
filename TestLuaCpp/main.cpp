90aa949172811a8232d8ada414903995267cb3d3
shin <funny.suo@gmail.com>
2014-06-24 16:55:24 +0800

    Init pro

---
+++ TestLuaCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestLuaCpp
+//
+//  Created by shin on 14/6/23.
+//  Copyright (c) 2014年 CocoaChina. All rights reserved.
+//
+
+#include <iostream>
+#include "lua.hpp"
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
